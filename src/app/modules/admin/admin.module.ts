31d02438c38daa13903fda069f0e2acf99aa9c50
Alessandro Capozzi <alessandro.capozzi@qwentes.it>
2022-07-11 12:56:00 +0200

    add menu component

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { ClientsListComponent } from './components/clients-list/clients-list.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    ClientsListComponent
+    ClientsListComponent,
+    MenuComponent
   ],
   imports: [
     CommonModule,
